1f7bd6e54b084e4b88c3b0a1f4f70f905f6b36d1
Xavier Pillons (MSFT) <xpillons@microsoft.com>
2024-04-26 16:49:22 +0200

    initial commit

---
+++ cluster-init/files/almalinux/rename_host.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+function enforce_hostname() {
+  local system_hostname=$1
+  local target_hostname=$2
+
+  logger -s "Ensure that the correct hostname has been set and update if necessary"
+  if [ "$system_hostname" != "$target_hostname" ]; then
+    logger -s "Warning: incorrect hostname ($system_hostname), it should be $target_hostname, updating"
+    hostname $target_hostname
+  fi
+  logger -s "Check in /etc/hosts"
+  hostname_in_hosts=$(getent hosts $(ifconfig eth0 | grep "inet " | xargs) | xargs | cut -d ' ' -f2 | tr '[:upper:]' '[:lower:]')
+  if [ "$hostname_in_hosts" != "$target_hostname" ]; then
+    logger -s "Warning: incorrect hostname ($hostname_in_hosts) in /etc/hosts, updating"
+    sed -i "s/$hostname_in_hosts/$target_hostname/ig" /etc/hosts
+  fi
+  logger -s "Check in /etc/hostname"
+  etc_hostname=$(</etc/hostname)
+  if [ "$etc_hostname" != "$target_hostname" ]; then
+    logger -s "Warning: incorrect /etc/hostname ($etc_hostname), it should be $target_hostname, updating"
+    echo $target_hostname > /etc/hostname
+  fi
+
+  logger -s "Restarting NetworkManager"
+  systemctl restart NetworkManager
+}
