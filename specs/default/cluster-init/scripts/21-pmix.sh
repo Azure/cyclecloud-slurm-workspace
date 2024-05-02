82a7526fdf7d2465742bfcb4134aa504b0df1765
Xavier Pillons (MSFT) <xpillons@microsoft.com>
2024-05-02 14:55:03 +0200

    use 3.2.5 for Ubuntu if PMIx not there

--- specs/default/cluster-init/scripts/21-pmix.sh
+++ specs/default/cluster-init/scripts/21-pmix.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -e
-PMIX_VERSION=4.2.9
 PMIX_ROOT=/opt/pmix
-PMIX_DIR=$PMIX_ROOT/$PMIX_VERSION
 
 logger -s "Configuring PMIx"
 
@@ -60,13 +58,16 @@ if [ ! -d $PMIX_ROOT ]; then
     os_release=$(cat /etc/os-release | grep "^ID\=" | cut -d'=' -f 2 | xargs)
     case $os_release in
         rhel|almalinux)
+            PMIX_VERSION=4.2.9
+            PMIX_DIR=$PMIX_ROOT/$PMIX_VERSION
             logger -s "Installing PMIx $PMIX_VERSION for $os_release"
             yum -y install pmix-$PMIX_VERSION-1.el8
             ;;
         ubuntu|debian)
             logger -s "Installing PMIx dependencies for $os_release"
+            PMIX_VERSION=3.2.5
+            PMIX_DIR=$PMIX_ROOT/$PMIX_VERSION
             apt-get update
-            #apt-get install -y git libevent-dev libhwloc-dev autoconf flex make gcc libxml2
             apt-get install -y libevent-dev libhwloc-dev
             build_pmix
             ln -s $PMIX_DIR/lib/libpmix.so /usr/lib/libpmix.so
