8de13dac958b06f533a87816dcd6180c9220a124
ryanhamel <ryan.hamel@cyclecomputing.com>
2024-06-07 08:26:04 -0400

    pass release tag directly to build.sh as a branch (#49)
    Co-authored-by: Ryan Hamel <ryan.hamel@microsoft.com>

--- release.sh
+++ release.sh
@@ -6,10 +6,9 @@ if [ "$1" == "" ] || [ "$1" == "-h" ]; then
 fi
 
 version=$1
-grep -q $version uidefinitions/createUiDefinition.json || (echo add $version to uidefinitions/createUiDefinition.json; exit 1)
-grep "param project_version" bicep/mainTemplate.bicep | grep -q $version || (echo set project_version = $version in bicep/mainTemplate.bicep)
-grep -q $version project.ini || (echo add $version to project.ini; exit 1)
+grep -F -q $version uidefinitions/createUiDefinition.json || (echo add $version to uidefinitions/createUiDefinition.json; exit 1)
+grep "param project_version" bicep/mainTemplate.bicep | grep -F -q $version || (echo set project_version = $version in bicep/mainTemplate.bicep)
+grep -F -q $version project.ini || (echo add $version to project.ini; exit 1)
 git tag $version
 git push origin $version
-git checkout $version -b $version
-./build.sh
+./build.sh $version
