0ca288006cd2cca3feb415a4630f13ac6822f6b7
ryanhamel <ryan.hamel@cyclecomputing.com>
2024-08-30 10:45:52 -0400

    Add --vnet-address-spaces and do not touch insidersBuild if -i is not passed in (#106)
    Co-authored-by: Ryan Hamel <ryan.hamel@microsoft.com>

--- util/deploy_sandbox_params.py
+++ util/deploy_sandbox_params.py
@@ -7,12 +7,7 @@ import sys
 """
 This is a simple development utility to override common settings when deploying
 multiple slurm workspaces via sandbox UI parameters. This will also set the branch correctly.
-Note that we use a -[letter] naming scheme for the resource groups. This is to ensure
-we create a non-overlapping vnet. For example
-my-rg-a -> 10.1.0.0/24
-my-rg-b -> 10.2.0.0/24
-...
-my-rg-z -> 10.26.0.0/24
+
 python3 deploy_sandbox_params.py --sandbox-ui-json raw-ui-parameters.json\
                                  --location southcentralus\
                                  --execute-vm-size Standard_F2s_v2\
@@ -31,23 +26,17 @@ def main() -> None:
     parser.add_argument("--dry-run", action="store_true", default=False)
     parser.add_argument("-b", "--branch")
     parser.add_argument("-i", "--insiders", action="store_true", default=False)
-
+    parser.add_argument("-s", "--vnet-address-space")
 
     args = parser.parse_args()
 
     ui_params = json.load(open(args.sandbox_ui_json))
 
     if args.resource_group:
-        if not args.resource_group[-2] == "-" and args.resource_group[-1].isalpha():
-            print("Please add a sufix of -{single_letter}, i.e. ryhamelccsw-a")
-            print("This suffix should be unique per running deployment, because we use it")
-            print("to set the vnet's address space. i.e. -a 10.1.0.0/24, -b 10.2.0.0/24 and so on")
-            sys.exit(1)
         ui_params["resourceGroup"]["value"] = args.resource_group
 
-        suffix = args.resource_group.split("-")[-1][0]
-        second_octal = ord(suffix) - ord("a") + 1
-        ui_params["network"]["value"]["addressSpace"] = f"10.{second_octal}.0.0/24"
+    if args.vnet_address_space:
+        ui_params["network"]["value"]["addressSpace"] = args.vnet_address_space
 
     if args.branch:
         branch = args.branch
@@ -60,19 +49,23 @@ def main() -> None:
             return
 
     ui_params['branch'] = {'value': branch}
-    ui_params['insidersBuild'] = {'value': args.insiders}
+    if 'insidersBuild' in ui_params:
+        if args.insiders:
+            ui_params['insidersBuild']["value"] = args.insiders
+    else:
+        ui_params['insidersBuild'] = {'value': args.insiders}
 
     if args.location:
         ui_params["location"]["value"] = args.location
 
     if args.vm_size:
         ui_params["ccVMSize"]["value"] = args.vm_size
-        ui_params["schedulerNode"]["value"]["vmSize"] = args.vm_size
+        ui_params["schedulerNode"]["value"]["sku"] = args.vm_size
 
     if args.execute_vm_size:
-        ui_params["hpc"]["value"]["vmSize"] = args.execute_vm_size
-        ui_params["htc"]["value"]["vmSize"] = args.execute_vm_size
-        ui_params["gpu"]["value"]["vmSize"] = args.execute_vm_size
+        ui_params["hpc"]["value"]["sku"] = args.execute_vm_size
+        ui_params["htc"]["value"]["sku"] = args.execute_vm_size
+        ui_params["gpu"]["value"]["sku"] = args.execute_vm_size
 
     with open("util/testparam.json", "w") as fw:
         json.dump({
