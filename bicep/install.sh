7db5da17978c662bc033d91fa84f9a85fcdd23e9
abatallas <167922471+abatallas@users.noreply.github.com>
2024-08-30 11:13:16 -0400

    Delete CC parameters file containing secrets (#108)

--- bicep/install.sh
+++ bicep/install.sh
@@ -217,7 +217,7 @@ timeout 360s bash -c 'until (! ls /opt/cycle_server/config/data/*.txt); do sleep
 
 cyclecloud start_cluster "$SLURM_CLUSTER_NAME"
 echo "CC start_cluster successful"
-# rm -f slurm_params.json
+rm -f slurm_params.json
 echo "Deleted input parameters file" 
 #TODO next step: wait for scheduler node to be running, get IP address of scheduler + login nodes (if enabled)
 popd
