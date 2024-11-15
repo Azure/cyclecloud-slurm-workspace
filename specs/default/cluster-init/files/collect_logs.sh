579142344ff8c3f9201b89cf2d52a476534a8952
Andrew Batallas <abatallas@microsoft.com>
2024-11-15 14:37:16 -0500

    WIP: Centralization of logs

---
+++ specs/default/cluster-init/files/collect_logs.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+set -e
+
+# Set the path to store the logs on /sched
+cluster_name=$(grep ClusterName= /etc/slurm/slurm.conf | sed 's/.*= *//; s/[ \t\r\n]*$//')
+vm_name=$(hostname)
+path="/sched/$cluster_name/$vm_name"
+
+# User may optionally override default path
+while (( "$#" )); do
+  case $1 in
+    --path)
+      if [ -n "$2" ] && [ "${2:0:1}" != "-" ]; then
+        path="$2"
+        shift 2
+      else
+        echo "Error: --path requires a non-empty option argument."
+        exit 1
+      fi
+      ;;
+    *)
+      echo "Error: Invalid argument. Use --path"
+      exit 1
+      ;;
+  esac
+done
+
+# Remove any trailing / in given path
+path="${path%/}"
+
+source_dir="/opt/azurehpc/diagnostics"
+
+# Run the built-in script to gather diagnostics
+yes | bash "$source_dir"/gather_azhpc_vm_diagnostics.sh
+
+# Identify the log file created by the above script
+log_file_name=$(ls -t "$source_dir" | head -n 1 | sed 's/\.tar\.gz$//')
+# Move datetime to the beginning of the file name 
+final_log_file_name=$(echo "$log_file_name" | sed -E 's/^([^.]+)\.(.+)$/\2.\1/')
+
+# Create a temporary directory to store the logs before packaging them
+tmp_dir="/tmp/logs"
+mkdir -p "$tmp_dir" && cd "$tmp_dir"
+
+# Move the log archive to the temporary directory and extract it
+mv "$source_dir"/"$log_file_name".tar.gz .
+tar -xzf "$log_file_name".tar.gz && rm "$log_file_name".tar.gz
+
+# Create a subdirectory named "cluster" and add more logs to it
+cd "$log_file_name" && mkdir -p cluster && cd cluster
+set +e # in a failed node, some of these directories may not exist
+log_directories=(
+    "/var/log/slurm"
+    "/var/log/slurmctld"
+    "/var/log/slurmd"
+    "/opt/azurehpc/slurm/logs"
+    "/opt/cycle/jetpack/logs"
+)
+for dir in "${log_directories[@]}"; do
+  cp -a --parents "$dir" .
+done
+which scontrol && scontrol show config > slurm_config.txt
+which scontrol && scontrol show nodes > slurm_nodes.txt
+set -e
+
+# Re-package the logs into an archive at the path specified by the user and clean up
+cd ../..
+mkdir -p "$path"
+tar -czvf "$path"/"$final_log_file_name".tar.gz "$log_file_name" && rm -rf "$log_file_name"
+rm -rf "$tmp_dir"
+echo "Log archive created at $path/$final_log_file_name.tar.gz"
---
+++ specs/default/cluster-init/scripts/30-add_scheduled_event_handlers.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -ex
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+collect_logs_script="$script_dir/../files/collect_logs.sh" 
+
+mkdir -p /opt/cycle/jetpack/scripts
+
+cat << EOF > /opt/cycle/jetpack/scripts/onTerminate.sh
+bash -c ${collect_logs_script}
+
+EOF
+chmod 770 /opt/cycle/jetpack/scripts/onTerminate.sh
