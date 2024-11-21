4cde1589165588ddd530c451f72b8c17178889f4
Andrew Batallas <abatallas@microsoft.com>
2024-11-21 12:07:58 -0500

    Fix typo in cron script

--- specs/default/cluster-init/scripts/31-add_log_cleanup_cron.sh
+++ specs/default/cluster-init/scripts/31-add_log_cleanup_cron.sh
@@ -10,7 +10,7 @@ fi
 cluster_name=$(which scontrol > /dev/null && scontrol show config | grep -i "ClusterName" | sed 's/.*ClusterName *= *//' | tr -d '[:space:]')
 
 cat << EOF > /etc/cron.daily/node_log_cleanup
-!#/bin/bash
+#!/bin/bash
 set -e
 find /shared/$cluster_name/node_logs/ -type f -mtime +7 -exec rm -f {} \;
 EOF
