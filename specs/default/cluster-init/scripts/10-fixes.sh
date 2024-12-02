c37c7dc089d427c0c643f5244f2d6085ab7f247c
Xavier Pillons (MSFT) <xpillons@microsoft.com>
2024-12-02 15:30:36 +0100

    run azslurm scale if needed

--- specs/default/cluster-init/scripts/10-fixes.sh
+++ specs/default/cluster-init/scripts/10-fixes.sh
@@ -48,12 +48,11 @@ if is_scheduler; then
     fix_ndv5
     fix_generate_amd_devices
 
-    # We probably don't need to rerun the autoscaler at this time
-    # # If touch file exists, then Update autoscaler
-    # if [ -f $TOUCH_FILE ]; then
-    #     logger -s "Update autoscaler"
-    #     AZSLURM=$(which azslurm)
-    #     $AZSLURM scale
-    #     logger -s "Autoscaler updated"
-    # fi
+    # If touch file exists, then Update autoscaler
+    if [ -f $TOUCH_FILE ]; then
+        logger -s "Update autoscaler"
+        AZSLURM=$(which azslurm)
+        $AZSLURM scale
+        logger -s "Autoscaler updated"
+    fi
 fi
