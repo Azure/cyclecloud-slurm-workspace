6702a8dd4c61d101d83176a30fed650a973f4815
Andrew Batallas <abatallas@microsoft.com>
2025-01-15 18:02:50 -0500

    Add whitespace to CCW template generation script

--- bicep/files-to-load/create_ccw_template.py
+++ bicep/files-to-load/create_ccw_template.py
@@ -54,7 +54,7 @@ def insert_below(input_template, substring, insert_string):
     return '\n'.join(lines)
 
 # Deletes all lines between that of the first instance of start_substring (inclusive) and that of the first instance of end_substring (exclusive) in input_template
-def remove_lines(input_template, start_substring, end_substring=None):
+def remove_lines(input_template, start_substring, end_substring = None):
     lines = input_template.split('\n')
     start_index = None
     end_index = None
@@ -82,7 +82,7 @@ def remove_line(input_template, substring):
     return remove_lines(input_template, substring)
 
 # Replaces the first replace_count instances of lines containing substring in input_template with replacement_string
-def replace_line(input_template, substring, replacement_string,replace_count=1):
+def replace_line(input_template, substring, replacement_string, replace_count = 1):
     lines = input_template.split('\n')
     for i, line in enumerate(lines):
         if replace_count == 0:
@@ -93,11 +93,12 @@ def replace_line(input_template, substring, replacement_string,replace_count=1):
     return '\n'.join(lines)
 
 # Apply all nested functions to the input_template
-def apply_all_changes(input_template,funcs):
+def apply_all_changes(input_template, funcs):
     def get_line_number(func):
         if hasattr(func,'wrapped'):
             return func.wrapped.__code__.co_firstlineno
         return func.__code__.co_firstlineno
+
     all_line_numbers = [get_line_number(func) for func in funcs]
     assert len(all_line_numbers) == len(set(all_line_numbers)), 'Multiple functions map to the same line. Please use the wrapped attribute for decorators.'
     sorted_funcs = sorted(funcs, key=get_line_number)
@@ -115,110 +116,124 @@ def write_file(contents):
 def create_ccw_template(template_file):
     @deprecation_version()
     def rename_cluster(template_file):
-        return template_file.replace('cluster Slurm',f'cluster {CCW_TEMPLATE_NAME}')
+        return template_file.replace('cluster Slurm', f'cluster {CCW_TEMPLATE_NAME}')
     @deprecation_version() # decorator omitted below as these changes to the main template would occur as a group
     def add_node_tags_parameter(template_file):
         def insert_node_tags_reference(template_file):
-            return insert_below(template_file,'Azure.Identities',SINGLE_INDENT+'Tags = $NodeTags')
+            return insert_below(template_file, 'Azure.Identities', SINGLE_INDENT + 'Tags = $NodeTags')
+
         def insert_parameter_node_tags(template_file):
-            return insert_above(template_file,'[[parameters Auto-Scaling]]',read_file('util/parameter_NodeTags.txt'))
-        return apply_all_changes(template_file,[func for _, func in locals().items() if callable(func)])
+            return insert_above(template_file, '[[parameters Auto-Scaling]]', read_file('util/parameter_NodeTags.txt'))
+        return apply_all_changes(template_file, [func for _, func in locals().items() if callable(func)])
     @deprecation_version()
     def update_slurm_configuration(template_file):
         @deprecation_version()
         def remove_slurm_packages(template_file):
-            return remove_line(remove_line(template_file,'slurm.install_pkg'),'slurm.autoscale_pkg')
+            return remove_line(remove_line(template_file, 'slurm.install_pkg'), 'slurm.autoscale_pkg')
+
         @deprecation_version()
         def add_slurm_user_ids(template_file):
-            slurm_user_ids = ['slurm.user.uid = 11100','slurm.user.gid = 11100','munge.user.uid = 11101','munge.user.gid = 11101']
+            slurm_user_ids = ['slurm.user.uid = 11100', 'slurm.user.gid = 11100', 'munge.user.uid = 11101', 'munge.user.gid = 11101']
             for user_id in slurm_user_ids[::-1]:
-                template_file = insert_below(template_file,'slurm.version',DOUBLE_INDENT+user_id)
+                template_file = insert_below(template_file, 'slurm.version', DOUBLE_INDENT + user_id)
             return template_file
+
         @deprecation_version()
         def remove_slurm_accounting_storageloc(template_file):
-            return remove_line(template_file,'slurm.accounting.storageloc')
-        return apply_all_changes(template_file,[func for _, func in locals().items() if callable(func)])
-    # is cluster.identities.default a good anchor for making the changes in add_ccw_cluster_init?
+            return remove_line(template_file, 'slurm.accounting.storageloc')
+        return apply_all_changes(template_file, [func for _, func in locals().items() if callable(func)])
     @deprecation_version()
     def update_cluster_init_headers(template_file):
         def add_ccw_cluster_init(template_file):
             @deprecation_version()
             def relabel_cluster_init_headers(template_file):
-                cluster_init_headers = ['cyclecloud/slurm:default','cyclecloud/slurm:scheduler','cyclecloud/slurm:login','cyclecloud/slurm:execute']
+                cluster_init_headers = ['cyclecloud/slurm:default', 'cyclecloud/slurm:scheduler', 'cyclecloud/slurm:login', 'cyclecloud/slurm:execute']
                 aslurm_major_version = f'{AZSLURM_VERSION_PARSED[0]}.{AZSLURM_VERSION_PARSED[1]}.x'
                 for cluster_init_header in cluster_init_headers:
-                    template_file = replace_line(template_file,cluster_init_header,DOUBLE_INDENT+f'[[[cluster-init {cluster_init_header}:{aslurm_major_version}]]]')
+                    template_file = replace_line(template_file, cluster_init_header, DOUBLE_INDENT + f'[[[cluster-init {cluster_init_header}:{aslurm_major_version}]]]')
                 return template_file            
-            template_file = remove_lines(template_file,'cyclecloud/slurm:default','[[[')
-            template_file = insert_below(template_file,'cluster.identities.default',read_file('util/cluster_inits_slurm_ccw.txt'))
+            template_file = remove_lines(template_file, 'cyclecloud/slurm:default','[[[')
+            template_file = insert_below(template_file, 'cluster.identities.default', read_file('util/cluster_inits_slurm_ccw.txt'))
             template_file = relabel_cluster_init_headers(template_file)
             return template_file
         return add_ccw_cluster_init(template_file)
     @deprecation_version()
     def set_sched_to_persistent(template_file):
-        return replace_line(template_file,'Persistent = False',DOUBLE_INDENT+'Persistent = True')
+        return replace_line(template_file, 'Persistent = False', DOUBLE_INDENT + 'Persistent = True')
     @deprecation_version() # decorator omitted below as these changes to the main template would occur as a group
     def fix_execute_node_count_parameters(template_file):
         def fix_references_to_core_count_parameters(template_file):
-            template_file = replace_line(template_file,'$MaxHPCExecuteCoreCount',SINGLE_INDENT+'MaxCount = $MaxHPCExecuteNodeCount')
-            template_file = replace_line(template_file,'$MaxHTCExecuteCoreCount',SINGLE_INDENT+'MaxCount = $MaxHTCExecuteNodeCount')
+            template_file = replace_line(template_file, '$MaxHPCExecuteCoreCount', SINGLE_INDENT + 'MaxCount = $MaxHPCExecuteNodeCount')
+            template_file = replace_line(template_file, '$MaxHTCExecuteCoreCount', SINGLE_INDENT + 'MaxCount = $MaxHTCExecuteNodeCount')
             return template_file
+
         def remove_parameter_hpc_core_count(template_file):
-            return remove_lines(template_file,'[[[parameter MaxHPCExecuteCoreCount]]]','[[[')
+            return remove_lines(template_file, '[[[parameter MaxHPCExecuteCoreCount]]]', '[[[')
+
         def remove_parameter_htc_core_count(template_file):
-            return remove_lines(template_file,'[[[parameter MaxHTCExecuteCoreCount]]]','[[[')
+            return remove_lines(template_file, '[[[parameter MaxHTCExecuteCoreCount]]]', '[[[')
+
         def add_parameters_execute_node_counts(template_file):
-            return insert_above(template_file,'[[[parameter MaxDynamicExecuteCoreCount]]]',read_file('util/parameters_MaxExecuteNodeCounts.txt'))
-        return apply_all_changes(template_file,[func for _, func in locals().items() if callable(func)])
+            return insert_above(template_file, '[[[parameter MaxDynamicExecuteCoreCount]]]', read_file('util/parameters_MaxExecuteNodeCounts.txt'))
+        return apply_all_changes(template_file, [func for _, func in locals().items() if callable(func)])
     @deprecation_version() # decorator omitted below as these changes to the main template would occur as a group
     def add_gpu_nodearray_and_parameters(template_file):
         def insert_nodearray_gpu(template_file):
-            return insert_above(template_file,'[[nodearray htc]]',read_file('util/nodearray_gpu.txt'))
+            return insert_above(template_file, '[[nodearray htc]]', read_file('util/nodearray_gpu.txt'))
+
         def insert_parameter_gpu_machine_type(template_file):
-            return insert_above(template_file,'[[[parameter DynamicMachineType]]]',read_file('util/parameter_GPUMachineType.txt'))
+            return insert_above(template_file, '[[[parameter DynamicMachineType]]]', read_file('util/parameter_GPUMachineType.txt'))
+
         def insert_parameter_gpu_image_name(template_file): 
-            return insert_above(template_file,'[[[parameter DynamicImageName]]]',read_file('util/parameter_GPUImageName.txt'))
+            return insert_above(template_file, '[[[parameter DynamicImageName]]]', read_file('util/parameter_GPUImageName.txt'))
+
         def insert_parameter_gpu_use_low_prio(template_file): 
-            return insert_above(template_file,'[[[parameter DynamicUseLowPrio]]]',read_file('util/parameter_GPUUseLowPrio.txt'))
+            return insert_above(template_file, '[[[parameter DynamicUseLowPrio]]]', read_file('util/parameter_GPUUseLowPrio.txt'))
+
         def insert_parameter_gpu_spot_max_price(template_file): 
-            return insert_above(template_file,'[[[parameter DynamicUseLowPrio]]]',read_file('util/parameter_GPUSpotMaxPrice.txt'))
+            return insert_above(template_file, '[[[parameter DynamicUseLowPrio]]]', read_file('util/parameter_GPUSpotMaxPrice.txt'))
+
         def insert_parameter_gpu_cluster_init_specs(template_file):
-            return insert_above(template_file,'[[[parameter DynamicClusterInitSpecs]]]',read_file('util/parameter_GPUClusterInitSpecs.txt'))
-        return apply_all_changes(template_file,[func for _, func in locals().items() if callable(func)])
+            return insert_above(template_file, '[[[parameter DynamicClusterInitSpecs]]]', read_file('util/parameter_GPUClusterInitSpecs.txt'))
+        return apply_all_changes(template_file, [func for _, func in locals().items() if callable(func)])
     @deprecation_version() # decorator omitted below as these changes to the main template would occur as a group
     def update_login_nodearray_and_add_parameters(template_file):
         def fix_nodearray_login_image_name(template_file):
             end_marker = 'cluster-init cyclecloud/slurm:login'
-            template_file = remove_lines(template_file,'[[nodearray login]]',end_marker)
-            template_file = insert_above(template_file,end_marker,read_file('util/nodearray_login.txt'))
+            template_file = remove_lines(template_file, '[[nodearray login]]', end_marker)
+            template_file = insert_above(template_file, end_marker, read_file('util/nodearray_login.txt'))
             return template_file
+
         def insert_parameter_login_image_name(template_file): 
-            return insert_above(template_file,'[[[parameter HPCImageName]]]',read_file('util/parameter_LoginImageName.txt'))
+            return insert_above(template_file, '[[[parameter HPCImageName]]]', read_file('util/parameter_LoginImageName.txt'))
+
         def insert_parameter_login_cluster_init_specs(template_file):
-            return insert_above(template_file,'[[[parameter HTCClusterInitSpecs]]]',read_file('util/parameter_LoginClusterInitSpecs.txt'))
-        return apply_all_changes(template_file,[func for _, func in locals().items() if callable(func)])
+            return insert_above(template_file, '[[[parameter HTCClusterInitSpecs]]]', read_file('util/parameter_LoginClusterInitSpecs.txt'))
+        return apply_all_changes(template_file, [func for _, func in locals().items() if callable(func)])
     # Update network attached storage (NAS)-related UI text
     @deprecation_version()
     def update_nas_gui(template_file):
         def fix_disk_warnings(template_file):
             return replace_line(template_file,
                                 'switching an active cluster over to NFS or Lustre from Builtin will delete the shared disk',
-                                DOUBLE_INDENT+'Config.Template := "<p><b>Warning</b>: switching an active cluster over to NFS from Builtin will delete the shared disk.</p>"',
-                                replace_count=2)
-        # replace_count=2 because AML is not an accepted option for /sched and /shared, i.e., where this text appears in the original template.
+                                DOUBLE_INDENT + 'Config.Template := "<p><b>Warning</b>: switching an active cluster over to NFS from Builtin will delete the shared disk.</p>"',
+                                replace_count = 2)
+
+        # replace_count = 2 because AML is not an accepted option for /sched and /shared, i.e., where this text appears in the original template.
         def fix_nfs_address(template_file):
             return replace_line(template_file,
                                 'The IP address or hostname of the NFS server or Lustre FS',
-                                DOUBLE_INDENT+'Description = The IP address or hostname of the NFS server. Also accepts a list comma-separated addresses, for example, to mount a frontend load-balanced Azure HPC Cache.',
-                                replace_count=2)
-        # replace_count=2 because AML is an acceptable option for parameter AdditionalNFSType but not /sched and /shared, i.e., the first two NFS defined in the original template.
+                                DOUBLE_INDENT + 'Description = The IP address or hostname of the NFS server. Also accepts a list comma-separated addresses, for example, to mount a frontend load-balanced Azure HPC Cache.',
+                                replace_count = 2)
+
+        # replace_count = 2 because AML is an acceptable option for parameter AdditionalNFSType but not /sched and /shared, i.e., the first two NFS defined in the original template.
         def fix_nfs_type_options(template_file):
             return replace_line(template_file,
                                 '[Label="Azure Managed Lustre"',
-                                DOUBLE_INDENT+'Config.Entries := {[Label="External NFS"; Value="nfs"]}',
-                                replace_count=2)
-        return apply_all_changes(template_file,[func for _, func in locals().items() if callable(func)]) 
-    return apply_all_changes(template_file,[func for _, func in locals().items() if callable(func)])  
+                                DOUBLE_INDENT + 'Config.Entries := {[Label="External NFS"; Value="nfs"]}',
+                                replace_count = 2)
+        return apply_all_changes(template_file, [func for _, func in locals().items() if callable(func)]) 
+    return apply_all_changes(template_file, [func for _, func in locals().items() if callable(func)])  
 
 def main():
     parser = argparse.ArgumentParser(description='Generate the CycleCloud Workspace for Slurm template')
