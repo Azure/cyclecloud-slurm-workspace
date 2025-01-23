ff4ce715a7d35adf1d8cbec9c8c85a8e05c9bc86
Xavier Pillons (MSFT) <xpillons@microsoft.com>
2025-01-23 10:35:36 +0100

    new nvme persistet mount script

--- specs/default/cluster-init/scripts/08-nvme.sh
+++ specs/default/cluster-init/scripts/08-nvme.sh
@@ -2,32 +2,7 @@
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "$script_dir/../files/common.sh" 
 
-function setup_nvme_disks() {
-
-    # If /mnt/nvme is already mounted then return
-    grep -qs '/mnt/nvme ' /proc/mounts && return
-
-    NVME_DISKS_NAME=`ls /dev/nvme*n1`
-    NVME_DISKS=`ls -latr /dev/nvme*n1 | wc -l`
-
-    logger -s "Number of NVMe Disks: $NVME_DISKS"
-
-    if [ "$NVME_DISKS" == "0" ]
-    then
-        exit 0
-    else
-        mkdir -p /mnt/nvme
-        # Needed incase something did not unmount as expected. This will delete any data that may be left behind
-        mdadm  --stop /dev/md*
-        mdadm --create /dev/md12 -f --run --level 0 --name nvme --raid-devices $NVME_DISKS $NVME_DISKS_NAME
-        mkfs.xfs -f /dev/md12
-        mount /dev/md12 /mnt/nvme || exit 1
-    fi
-
-    chmod 1777 /mnt/nvme
-    logger -s "/mnt/nvme mounted"
-}
-
 if is_compute; then
-    setup_nvme_disks
+chmod +x $script_dir/../files/nvme_persistent_mount.sh
+    $script_dir/../files/nvme_persistent_mount.sh xfs /mnt/nvme
 fi
