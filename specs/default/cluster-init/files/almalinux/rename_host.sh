e267339ae35855092d3c463cd87c05b1783bebf3
Ryan Hamel <ryhamel@microsoft.com>
2025-05-07 14:46:23 -0400

    fix: remove fqdns from getent hosts in rename_hosts scripts

--- specs/default/cluster-init/files/almalinux/rename_host.sh
+++ specs/default/cluster-init/files/almalinux/rename_host.sh
@@ -10,7 +10,7 @@ function enforce_hostname() {
     hostname $target_hostname
   fi
   logger -s "Check in /etc/hosts"
-  hostname_in_hosts=$(getent hosts $(ifconfig eth0 | grep "inet " | xargs) | xargs | cut -d ' ' -f2 | tr '[:upper:]' '[:lower:]')
+  hostname_in_hosts=$(getent hosts $(ifconfig eth0 | grep "inet " | xargs) | xargs | cut -d ' ' -f2 | tr '[:upper:]' '[:lower:]') | cut -d'.' -f1
   if [ "$hostname_in_hosts" != "$target_hostname" ]; then
     logger -s "Warning: incorrect hostname ($hostname_in_hosts) in /etc/hosts, updating"
     sed -i "s/$hostname_in_hosts/$target_hostname/ig" /etc/hosts
