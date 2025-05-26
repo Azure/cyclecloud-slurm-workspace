fefd0bfff2c8e1b2e309ce44b6356cda44b377e3
Ryan Hamel <ryhamel@microsoft.com>
2025-05-26 11:58:54 -0400

    WIP: tweak spoke to use outputs/ dir

--- bicep/hub/deploy_spoke.sh
+++ bicep/hub/deploy_spoke.sh
@@ -60,25 +60,48 @@ if az deployment sub show -g -n "${SPOKE_DEPLOYMENT_NAME}" > /dev/null 2>&1; the
 fi
 
 fetch_outputs() {
-az deployment group show -g "$HUB_RG_NAME" -n "hub-vnet${SUFFIX}" --query properties.outputs > hub-vnet-outputs.json
-az deployment group show -g "$HUB_RG_NAME" -n "hub-anf-resources${SUFFIX}" --query properties.outputs > hub-anf-outputs.json
-az deployment group show -g "$HUB_RG_NAME" -n "hub-db${SUFFIX}" --query properties.outputs > hub-db-outputs.json
-[ -f cyclecloud-monitoring/infra/outputs.json ] && cp cyclecloud-monitoring/infra/outputs.json hub-monitoring-outputs.json
-# az deployment group show -g "$HUB_RG_NAME" -n ingestionEndpoint --query properties.outputs > hub-monitoring-outputs.json
+    mkdir -p outputs
+    if [ -f outputs/hub-vnet-outputs.json ]; then
+        echo "outputs/hub-vnet-outputs.json already fetched. Skipping."
+    else
+        echo "Fetching outputs for hub vnet..."
+        az deployment group show -g "$HUB_RG_NAME" -n "hub-vnet${SUFFIX}" --query properties.outputs > outputs/hub-vnet-outputs.json
+    fi
+
+    if [ -f outputs/hub-anf-outputs.json ]; then
+        echo "outputs/hub-anf-outputs.json already fetched. Skipping."
+    else
+        echo "Fetching outputs for hub ANF..."
+        az deployment group show -g "$HUB_RG_NAME" -n "hub-anf-resources${SUFFIX}" --query properties.outputs > outputs/hub-anf-outputs.json
+    fi
+    if [ -f outputs/hub-db-outputs.json ]; then
+        echo "outputs/hub-db-outputs.json already fetched. Skipping."
+    else
+        echo "Fetching outputs for hub MySQL database..."
+        az deployment group show -g "$HUB_RG_NAME" -n "hub-db${SUFFIX}" --query properties.outputs > outputs/hub-db-outputs.json
+    fi
+    if [ -f outputs/hub-monitoring-outputs.json ]; then
+        echo "outputs/hub-monitoring-outputs.json already fetched. Skipping."
+    else
+        echo "Fetching outputs for hub monitoring..."
+        [ -f cyclecloud-monitoring/infra/outputs.json ] && cp cyclecloud-monitoring/infra/outputs.json outputs/hub-monitoring-outputs.json
+        # az deployment group show -g "$HUB_RG_NAME" -n ingestionEndpoint --query properties.outputs > outputs/hub-monitoring-outputs.json
+    fi
+    echo "Done fetching outputs."
 }
 
 fetch_outputs
 
-cp original_spoke_params.json spoke_params.json
+cp original_spoke_params.json params/spoke_params.json
 
 replace_fields() {
-    jq "$1" spoke_params.json > tmp_spoke_params.json && mv tmp_spoke_params.json spoke_params.json
+    jq "$1" params/spoke_params.json > tmp_spoke_params.json && mv tmp_spoke_params.json params/spoke_params.json
 }
 
 # shared FS
-IP_ADDRESS=$(jq -r '.ipAddress.value' hub-anf-outputs.json)
-EXPORT_PATH=$(jq -r '.exportPath.value' hub-anf-outputs.json)
-MOUNT_OPTIONS=$(jq -r '.mountOptions.value' hub-anf-outputs.json)
+IP_ADDRESS=$(jq -r '.ipAddress.value' outputs/hub-anf-outputs.json)
+EXPORT_PATH=$(jq -r '.exportPath.value' outputs/hub-anf-outputs.json)
+MOUNT_OPTIONS=$(jq -r '.mountOptions.value' outputs/hub-anf-outputs.json)
 replace_fields ".sharedFilesystem={ value: { type: \"nfs-existing\", ipAddress: \"$IP_ADDRESS\", exportPath: \"$EXPORT_PATH\", mountOptions: \"$MOUNT_OPTIONS\" } }"
 
 # new vnet 
@@ -86,20 +109,20 @@ ADDRESS_SPACE="10.${SPOKE_NUMBER}.0.0/24"
 replace_fields ".network.value.addressSpace=\"$ADDRESS_SPACE\""
 
 # vnet to peer 
-PEERED_VNET_ID=$(jq -r '.vnetId.value' hub-vnet-outputs.json)
+PEERED_VNET_ID=$(jq -r '.vnetId.value' outputs/hub-vnet-outputs.json)
 PEERED_VNET_NAME=$(echo "${PEERED_VNET_ID}" | cut -d '/' -f9)
 PEERED_VNET_LOCATION=$(az network vnet show -g "$RG" -n "$PEERED_VNET_NAME" --query location -o tsv | tr -d '\r\n')
 replace_fields ".network.value.vnetToPeer={ name: \"$PEERED_VNET_NAME\", id: \"$PEERED_VNET_ID\", location: \"$PEERED_VNET_LOCATION\", subscriptionName: \"\"}"
 
 # database config
 DB_IP="10.0.0.228"
-DB_USERNAME=$(jq -r '.adminUser.value' db_params.json)
-DB_PASSWORD=$(jq -r '.adminPassword.value' db_params.json)
+DB_USERNAME=$(jq -r '.adminUser.value' params/db_params.json)
+DB_PASSWORD=$(jq -r '.adminPassword.value' params/db_params.json)
 replace_fields ".databaseConfig={ value: { type: \"privateIp\", databaseUser: \"$DB_USERNAME\", privateIp: \"$DB_IP\" }}"
 replace_fields ".databaseAdminPassword={ value: \"$DB_PASSWORD\" }"
 
 # monitoring 
-MONITORING_INGESTION_ENDPOINT=$([ -f hub-monitoring-outputs.json ] && jq -r '.properties.outputs.ingestionEndpoint.value' hub-monitoring-outputs.json || echo "")
+MONITORING_INGESTION_ENDPOINT=$([ -f outputs/hub-monitoring-outputs.json ] && jq -r '.properties.outputs.ingestionEndpoint.value' outputs/hub-monitoring-outputs.json || echo "")
 
 az deployment sub create \
     --location "$LOCATION" \
