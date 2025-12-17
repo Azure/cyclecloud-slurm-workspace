64f6aca5fb143c3b25c54ab03a4fb86d2ca0428b
Andrew Batallas <abatallas@microsoft.com>
2025-12-17 11:44:10 -0500

    Ensure latest tag version is used for release

--- release.sh
+++ release.sh
@@ -6,9 +6,9 @@ if [ "$1" == "" ] || [ "$1" == "-h" ]; then
 fi
 
 version=$1
+git fetch origin --tags --force
+git checkout $version
 grep -F -q $version uidefinitions/createUiDefinition.json || (echo add $version to uidefinitions/createUiDefinition.json; exit 1)
 grep "param projectVersion" bicep/mainTemplate.bicep | grep -F -q $version || (echo set project_version = $version in bicep/mainTemplate.bicep)
 grep -F -q $version project.ini || (echo add $version to project.ini; exit 1)
-git tag $version
-git push origin $version
 ./build.sh $version
